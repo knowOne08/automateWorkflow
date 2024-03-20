a1c341f4e4ae31594883290f90129f6baa340f05
Yash Darji <yashdarji23082004@gmail.com>
2024-03-20 16:22:53 +0530

    Verification function update and it works

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ const imap = new Imap({
   }
 });
 
+const browser_verification = async (link) => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.goto(link);
+
+
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Tab');
+  await page.keyboard.press('Enter');
+  await browser.close();
+}
+
 imap.once('ready', () => {
   console.log("Chale che");
   imap.openBox('INBOX', false, (err, box) => {
@@ -27,60 +42,18 @@ imap.once('ready', () => {
         msg.on('body', async (stream) => {
           const parsed = await simpleParser(stream);
           const { from, subject, text } = parsed;
-
-          // console.log(from.value[0].address)
-          // console.log(subject)
-          // if (from.text === 'info@account.netflix.com' && subject === 'Important: How to update your Netflix household') {
           if (from.value[0].address == 'info@account.netflix.com' && subject == 'Important: How to update your Netflix household') {
 
             const linkRegex = /https:\/\/www\.netflix\.com\/account\/update-primary-location\?[^'"\s]+/;
             const matches = text.match(linkRegex);
-            // const linkRegex = /https:\/\/youtube\.com/; // Example regex
-
-            console.log(matches)
-
-
+            
             if (matches) {
-              const link = matches[0];
-              console.log('Complete Link:', link);
-              const browser = await puppeteer.launch({ headless: true });
-              const page = await browser.newPage();
-              await page.goto(link); //goto verification page
-
-              await page.waitForSelector('button');
-              console.log("here")
-              // Click the button with the text "Update Netflix Household"
-              await page.evaluate(() => {
-                try {
-                  console.log("here");
-                  const buttons = document.querySelectorAll('button');
-                  let buttonClicked = false;
-              
-                  buttons.forEach(button => {
-                    if (button.innerText.trim() === 'Update Netflix Household') {
-                      button.click();
-                      console.log("Button clicked");
-                      buttonClicked = true;
-                    }
-                  });
-              
-                  if (!buttonClicked) {
-                    console.log("Button not found or not clicked");
-                  }
-                } catch (error) {
-                  console.error('Error in page.evaluate:', error);
-                }
-              
-              });
-                page.waitForNavigation({ timeout: 5000 }),
-                await browser.close();
-              } else {
-                console.log('No matching link found.');
-              }
-
-            // Example: Click on the first link containing "verification link"
+              await browser_verification(matches[0]);
+            } else {
+              console.log('No matching link found.');
+            }
           }
-          });
+        });
       });
 
       fetch.once('error', (err) => {
@@ -95,3 +68,5 @@ imap.once('error', (err) => {
 });
 
 imap.connect();
+
+
